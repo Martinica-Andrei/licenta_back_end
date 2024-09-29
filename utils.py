b0638820c598c0f57c37ac06702d22c38dc36a67
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2024-09-29 17:48:55 +0300

    finished scraping book covers

--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
 import re
+from hashlib import sha256
+from pathlib import Path
+
 
 def escape_fts(str):
     return re.sub(r'[\'\"]', '', str)
+
+DATASETS_AMAZON_IMAGES_PATH = Path('datasets/amazon/images')
+
+def convert_str_to_datasets_amazon_images_path(x):
+    x = sha256(x.encode()).hexdigest()
+    x = x + '.jpg'
+    x = DATASETS_AMAZON_IMAGES_PATH / x
+    return x
