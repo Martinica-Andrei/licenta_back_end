7457fd413b363eaf43390354d038b6a3d85fba91
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2024-10-13 11:49:04 +0300

    changed database with mysql and  added fulltext to book_data

--- db_models/book_data.py
+++ db_models/book_data.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Text, Index
 from .base import Base
 
 class BookData(Base):
@@ -6,8 +6,12 @@ class BookData(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String(1_000), nullable=False)
-    description = Column(String(50_000), nullable=True)
+    description = Column(Text(50_000), nullable=True)
     authors = Column(String(5000), nullable=True)
     previewlink = Column(String(2000), nullable=True)
     infolink = Column(String(2000), nullable=True)
     categories = Column(String(2000), nullable=True)
+
+    __table_args__ = (
+        Index('ix_fulltext_title', 'title', mysql_prefix='FULLTEXT'),
+    )
