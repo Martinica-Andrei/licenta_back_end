3427b4069f6bd35aa415679512d02a4e2bb1cfee
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2025-02-16 12:06:15 +0200

    removed session module - small change

--- session.py
+++ session.py
@@ -1,12 +1,12 @@
 from flask_session import Session
 
 def init_session(app, db):
-    app.config['SESSION_TYPE'] = 'sqlalchemy'
-    app.config['SESSION_SQLALCHEMY'] = db
+    #app.config['SESSION_TYPE'] = 'sqlalchemy'
+    #app.config['SESSION_SQLALCHEMY'] = db
     app.config['SESSION_COOKIE_HTTPONLY'] = True
-    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_COOKIE_SECURE'] = False
     app.config['SESSION_COOKIE_SAMESITE'] = 'none'
     app.config['SESSION_COOKIE_PARTITIONED'] = True
     # if true, each request refreshes the expiration date of token
     app.config['SESSION_REFRESH_EACH_REQUEST'] = False
-    Session(app)
+    #Session(app)
