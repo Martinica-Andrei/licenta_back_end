ff2d83debeeb95b15736cbbf143d092ed2b4372f
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2025-06-05 10:14:55 +0300

    refactored alembic migrations, but they don't work right now

--- alembic/versions/882e2b40fcbc_download_data_and_seed.py
+++ alembic/versions/882e2b40fcbc_download_data_and_seed.py
@@ -24,59 +24,150 @@ down_revision: Union[str, None] = 'afa90328bf9b'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
+
 def upgrade() -> None:
-
-    download_books_data()
-
-    authors = pd.read_csv(utils.BOOKS_AUTHORS).rename(columns={'author_id' : 'id'})
-    authors = authors.loc[:, ["id","name"]]
-
-    categories = pd.read_csv(utils.BOOKS_CATEGORIES).rename(columns={'0':'name'})
-    categories_encoder = LabelEncoder().fit(categories['name'])
-    categories['id'] = categories_encoder.transform(categories['name'])
-
     connection = op.get_bind()
 
-    authors.to_sql('author', connection, if_exists='append', index=False)
-    categories.to_sql('category', connection, if_exists='append', index=False)
+    download_books_data()
+    authors_to_sql(connection)
+
+    categories_encoder = LabelEncoder()  # also maps categories from book_data
+    categories_to_sql(connection, categories_encoder)
 
     book_df = pd.read_csv(utils.BOOKS_DATA_BOOKS_PROCESSED)
-   
-    book_authors = book_df['authors'].apply(ast.literal_eval).reset_index()
-    book_authors = pd.concat(book_authors.apply(convert_book_authors, axis=1).values, axis=0, ignore_index=True)
-
-    book_categories = book_df['categories'].apply(ast.literal_eval).reset_index()
-    book_categories = pd.concat(book_categories.apply(convert_book_categories, axis=1).values, axis=0, ignore_index=True)
-    book_categories['category_id'] = categories_encoder.transform(book_categories['category_id'])
-
-    book_df = book_df[['title', 'description', 'url', 'image_url']].copy()
-    book_df['description'] = book_df['description'].fillna('')
-
-    no_photo_link = 'https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png'
-    book_df['image_url'] = book_df['image_url'].replace({no_photo_link: np.nan})
-
-    base_url = 'https://images.gr-assets.com/books/'
-    book_df['image_url'] = book_df['image_url'].str.replace(base_url, '').str.replace('/', '-')
-
-    book_df.rename(columns={"url" : "link", "image_url" : "image_link"}, inplace=True)
-    book_df.index.name = 'id'
-
-    book_df.to_sql('book', connection, if_exists='append')
-    book_categories.to_sql('book_categories', connection, if_exists='append', index=False)
-    book_authors.to_sql('book_authors', connection, if_exists='append', index=False)
+    book_authors_to_sql(connection, book_df)
+    book_categories_to_sql(connection, book_df, categories_encoder)
+    book_to_sql(connection, book_df)
 
 
 def downgrade() -> None:
     pass
 
+
+def authors_to_sql(connection) -> None:
+    """
+    Converts authors to sql.
+
+    Args:
+        connection (Connection) : Sql connection.
+
+    Returns:
+        None.
+    """
+    authors = pd.read_csv(utils.BOOKS_AUTHORS).rename(
+        columns={'author_id': 'id'})
+    authors = authors.loc[:, ["id", "name"]]
+    authors.to_sql('author', connection, if_exists='append', index=False)
+
+
+def categories_to_sql(connection, categories_enc: LabelEncoder) -> None:
+    """
+    Converts categories to sql.
+
+    Args:
+        connection (Connection): Sql connection.
+        categories_enc (LabelEncoder): Label encoder encodes categories names.
+
+    Returns:
+        None.
+    """
+    categories = pd.read_csv(utils.BOOKS_CATEGORIES).rename(
+        columns={'0': 'name'})
+    categories_enc.fit(categories['name'])
+    categories['id'] = categories_enc.transform(categories['name'])
+    categories.to_sql('category', connection, if_exists='append', index=False)
+
+
+def book_authors_to_sql(connection, book_df: pd.DataFrame) -> None:
+    """
+    Converts book authors to sql.
+
+    Args:
+        connection (Connection): Sql connection.
+        book_df (pd.DataFrame): Dataframe containing 'authors' column.
+
+    Returns:
+        None.
+    """
+    # ast.literal_eval converts to dict from str
+    # reset_index to obtain a new column called index containing each row number
+    book_authors = book_df['authors'].apply(ast.literal_eval).reset_index()
+    # for each row, create a new dataframe mapping each author with each book index
+    # and concatenate all dataframes vertically
+    book_authors = pd.concat(book_authors.apply(
+        convert_book_authors, axis=1).values, axis=0, ignore_index=True)
+    print(book_authors)
+    book_authors.to_sql('book_authors', connection,
+                        if_exists='append', index=False)
+
+
+def book_categories_to_sql(connection, book_df: pd.DataFrame, categories_enc: LabelEncoder):
+    """
+    Converts book categories to sql.
+
+    Args:
+        connection (Connection): Sql connection.
+        book_df (pd.DataFrame): Dataframe containing 'categories' column.
+
+    Returns:
+        None.
+    """
+    # ast.literal_eval converts to array from str
+    # reset_index to obtain a new column called index containing each row number
+    book_categories = book_df['categories'].apply(
+        ast.literal_eval).reset_index()
+    # for each row, create a new dataframe mapping each category with each book index
+    # and concatenate all dataframes vertically
+    book_categories = pd.concat(book_categories.apply(
+        convert_book_categories, axis=1).values, axis=0, ignore_index=True)
+    # map category names to ids using categories_enc
+    book_categories['category_id'] = categories_enc.transform(
+        book_categories['category_id'])
+    book_categories.to_sql('book_categories', connection,
+                           if_exists='append', index=False)
+
+
+def book_to_sql(connection, book_df: pd.DataFrame):
+    """
+    Converts book to sql.
+
+    Args:
+        connection (Connection): Sql connection.
+        book_df (pd.DataFrame): Dataframe to be converted to sql.s
+
+    Returns:
+        None.
+    """
+    book_df = book_df[['title', 'description', 'url', 'image_url']].copy()
+    book_df['description'] = book_df['description'].fillna('')
+
+    book_df['image_url'] = utils.replace_no_photo_link(book_df['image_url'])
+    book_df['image_url'] = book_df['image_url'].apply(
+        utils.process_book_scraped_url)
+
+    book_df.rename(
+        columns={"url": "link", "image_url": "image_link"}, inplace=True)
+    book_df.index.name = 'id'
+
+    book_df.to_sql('book', connection, if_exists='append')
+
+
 def convert_book_categories(x):
+    # index contains row numbers
     index, categories = x['index'], x['categories']
+    # for each category, create a list containing same row number and category
     categories = [[index] + [v] for v in categories]
     df = pd.DataFrame(categories, columns=['book_id', 'category_id'])
     return df
 
-def convert_book_authors(x):
+
+def convert_book_authors(x) -> pd.DataFrame:
+    """
+    Converts book authors in a single row into a dataframe containing rows for each author and same book_id.
+    """
+    # index contains row number
     index, authors = x['index'], x['authors']
+    # create a new dataframe repeating the same row index for each author
     authors = [[index] + [k, v] for k, v in authors.items()]
     df = pd.DataFrame(authors, columns=['book_id', 'author_id', 'role'])
     df['role'] = df['role'].replace({'': np.nan})
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+
 BOOKS_DATA = Path('books_data')
 BOOKS_DATA_MODEL = BOOKS_DATA / 'model_adagrad_200.pkl'
 BOOKS_DATA_ITEM_FEATURES = BOOKS_DATA / 'item_features.npz'
@@ -13,5 +16,34 @@ BOOKS_CATEGORIES = BOOKS_DATA / 'categories.csv'
 BOOKS_DATA_USER_PREPROCESSING = BOOKS_DATA / 'user_processing.pkl'
 BOOKS_DATA_ITEM_PREPROCESSING = BOOKS_DATA / 'item_preprocessing.pkl'
 
-def string_list_to_list(x): 
+
+def string_list_to_list(x):
     return [item.strip(' \'"') for item in x.strip('[]').split(',')]
+
+
+def replace_no_photo_link(s: pd.Series) -> pd.Series:
+    """
+    Replaces `s` pd.Series no photo link with np.nan
+
+    Args:
+        s (pd.Series): Pandas series to replace no photo link
+
+    Returns:
+        pd.Series: Series with no photo link replaced with np.nan.
+    """
+    no_photo_link = 'https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png'
+    return s.replace({no_photo_link: np.nan})
+
+__base_url = 'https://images.gr-assets.com/books/'
+
+def process_book_scraped_url(url : str) -> str:
+    """
+    Processes `url` by removing leaving only name and replaces / with - ."
+
+    Args:
+        url (str): Url to process.
+    
+    Returns:
+        str: Processed url.s
+    """
+    return url.replace(__base_url, '').replace('/', '-')
